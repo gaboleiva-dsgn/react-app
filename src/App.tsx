be41b73ed4388536423dd38f81ba57f7179f8c00
Gabo Leiva <g.leivach@gmail.com>
2024-07-16 15:08:22 -0400

    Se crea componente List

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 
 function App() {
   return (
     <Card>
       <CardBody title="Titulo de card" text="Este es el texto de card" />
+      <List />
     </Card>
   );
 }
